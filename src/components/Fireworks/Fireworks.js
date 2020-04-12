4a91e57348edc71c6456971a8b1dc2f1587e8834
Rafael Barbosa <rafaelbarbosadc@gmail.com>
2020-04-11 20:23:41 -0400

    Fireworks now DAMN

---
+++ src/components/Fireworks/Fireworks.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Fireworks.css";
+
+export default function Fireworks() {
+  return (
+    <div class="pyro">
+      <div class="before"></div>
+      <div class="after"></div>
+    </div>
+  );
+}
